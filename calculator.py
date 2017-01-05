f47075c832a01f308de7a33405c5f1308631b4e7
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 17:14:53 -0800

    The second part of the calculator

---
+++ calculator.py
@@ -0,0 +1,36 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+while True:
+    input_string = raw_input()
+    token = input_string.split(" ")
+    if token[0] == 'q':
+        break
+    elif token[0] == "+":
+        print add(int(token[1]), int(token[2])) 
+
+    elif token[0] == '-':
+        print subtract(int(token[1]), int(token[2]))
+    elif token[0] == '*':
+        print multiply(int(token[1]), int(token[2]))
+    elif token[0] == '/':
+        print divide(int(token[1]), int(token[2]))
+    elif token[0] == 'square':
+        print square(int(token[1]))
+    elif token[0] == 'cube':
+        print cube(int(token[1]))
+    elif token[0] == 'power':
+        print power(int(token[1]), int(token[2]))
+    elif token[0] == 'mod':
+        print mod(int(token[1]), int(token[2]))
+    else:
+        print "I dont understand"
+
+
