48583b6e29ebd26783a6cf3286161763a3b7dc2e
Cristina Rodriguez <yosola@gmail.com>
2017-01-05 23:28:23 -0800

    The calculator excersise

--- arithmetic.py
+++ arithmetic.py
@@ -2,15 +2,33 @@ def add(num1, num2):
     """Return the sum of two numbers"""
     return num1 + num2
 
+def add_from_list(input_list):
+    sum_num = int(input_list[1])
+    for number in range(2, len(input_list)):
+        sum_num = sum_num + int(input_list[number])
+    return sum_num
+
 
 def subtract(num1, num2):
     """Return the difference of two numbers"""
     return num1 - num2
 
+def subtrac_from_list(input_list):
+    sub_num = int(input_list[1])
+    for number in range(2, len(input_list)):
+        sub_num = sub_num - int(input_list[number])
+    return sub_num
+
 
 def multiply(num1, num2):
     """Return the product of two numbers"""
     return num1 * num2
+
+def multiply_from_list(input_list):
+    multiply_num = int(input_list[1])
+    for number in range(2, len(input_list)):
+        multiply_num = multiply_num * int(input_list[number])
+    return multiply_num
 
 
 def divide(num1, num2):
--- calculator.py
+++ calculator.py
@@ -10,26 +10,64 @@ from arithmetic import *
 
 while True:
     input_string = raw_input()
-    token = input_string.split(" ")
-    if token[0] == 'q':
-        break
-    elif token[0] == "+":
-        print add(int(token[1]), int(token[2])) 
+    tokens = input_string.split(" ")
 
-    elif token[0] == '-':
-        print subtract(int(token[1]), int(token[2]))
-    elif token[0] == '*':
-        print multiply(int(token[1]), int(token[2]))
-    elif token[0] == '/':
-        print divide(int(token[1]), int(token[2]))
-    elif token[0] == 'square':
-        print square(int(token[1]))
-    elif token[0] == 'cube':
-        print cube(int(token[1]))
-    elif token[0] == 'power':
-        print power(int(token[1]), int(token[2]))
-    elif token[0] == 'mod':
-        print mod(int(token[1]), int(token[2]))
+    if len(tokens) < 3 and tokens[0] == "+":
+        print "I can't make any operation with just a number"
+    elif len(tokens) == 3 and tokens[0] == "+":
+        print add(int(tokens[1]), int(tokens[2]))
+    elif len(tokens) > 3 and tokens[0] == "+":
+        print add_from_list(tokens)
+
+    elif len(tokens) < 3 and tokens[0] == "-":
+        print "I can't make any operation with just a number"
+    elif len(tokens) == 3 and tokens[0] == "-":
+        print subtract(int(tokens[1]), int(tokens[2]))
+    elif len(tokens) > 3 and tokens[0] == "-":
+        print subtrac_from_list(tokens)
+
+    elif len(tokens) < 3 and tokens[0] == "*":
+        print "I can't make any operation with just a number"
+    elif len(tokens) == 3 and tokens[0] == "*":
+        print multiply(int(tokens[1]), int(tokens[2]))
+    elif len(tokens) > 3 and tokens[0] == "*":
+        print multiply_from_list(tokens)
+
+    elif len(tokens) < 3 and tokens[0] == "/":
+        print "I can't make any operation with just a number"
+    elif len(tokens) == 3 and tokens[0] == '/':
+        print divide(int(tokens[1]), int(tokens[2]))
+    elif len(tokens) > 3 and tokens[0] == "/":
+        print "I can't make this operation with more that two numbers"
+
+    elif len(tokens) == 1 and tokens[0] == "square":
+        print "I can't make this operation without a number"
+    elif len(tokens) < 3 and tokens[0] == "square":
+        print square(int(tokens[1]))
+    elif len(tokens) >= 3 and tokens[0] == 'square':
+        print "I can't make this operation with more that two numbers"
+    
+    elif len(tokens) == 1 and tokens[0] == "cube":
+        print "I can't make this operation without a number"
+    elif len(tokens) < 3 and tokens[0] == "cube":
+        print cube(int(tokens[1]))
+    elif len(tokens) >= 3 and tokens[0] == 'cube':
+        print "I can't make this operation with more that two numbers"
+
+    elif len(tokens) < 3 and tokens[0] == "power":
+        print "I can't make this operation without a number"
+    elif len(tokens) == 3 and tokens[0] == 'power':
+        print power(int(tokens[1]), int(tokens[2]))
+    elif len(tokens) > 3 and tokens[0] == "power":
+        print "I can't make this operation with more that two numbers"
+
+    elif len(tokens) < 3 and tokens[0] == "mod":
+        print "I can't make this operation without a number"
+    elif len(tokens) == 3 and tokens[0] == 'mod':
+        print mod(int(tokens[1]), int(tokens[2]))
+    elif len(tokens) > 3 and tokens[0] == "mod":
+        print "I can't make this operation with more that two numbers"
+
     else:
         print "I dont understand"
 
